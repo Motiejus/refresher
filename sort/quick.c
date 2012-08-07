4d2dda1dff9992d7244f2d930baa8f81af9ac9a4
Motiejus Jakštys <desired.mta@gmail.com>
2012-08-07 23:44:10 +0200

    Fix quicksort

--- sort/quick.c
+++ sort/quick.c
@@ -8,22 +8,22 @@ static void swap(Itm *a, Itm *b) {
 }
 
 static int partition(Itm *arr, int left, int right) {
-    int pivotIdx = left;
-
-    printf("\nLeft: %d, Right: %d, Values: %ld %ld", left, right, *(arr + left), *(arr + right));
+    int pivotIdx = right;
 
     while (left < right) {
         while (arr[left] <= arr[pivotIdx]) left++;
         while (arr[right] > arr[pivotIdx]) right--;
         if (left < right) swap(&arr[left], &arr[right]);
     }
+    arr[right] = arr[pivotIdx];
+    arr[pivotIdx] = arr[right];
     return pivotIdx;
 }
 
 static void quick_sort_f(Itm *arr, int left, int right) {
     int pivotIdx = partition(arr, left, right);
     if (left < right) {
-        quick_sort_f(arr, left, pivotIdx);
+        quick_sort_f(arr, left, pivotIdx - 1);
         quick_sort_f(arr, pivotIdx + 1, right);
     }
 }
